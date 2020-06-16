592e982339e9289a6b547201b97966c35363fab3
Павел Васильев <Pavel.Vasilyev@itspro.by>
2020-06-16 09:48:11 +0300

    test script

--- js/main.js
+++ js/main.js
@@ -2,15 +2,22 @@ $(document).ready(function () {
   let button = $('.price__button');
 
   $(button).click(function () { 
-    let imgSrc = $(this).parent().parent().parent().find('.test-image').attr('src');
+    let imgSrc = $(this).parent().parent().parent().find('.minicard__image').attr('src');
     let titleCard = $(this).parent().parent().find('.info__title--text').text();
     let adressCard = $(this).parent().parent().find('.adress').text();
     let guestsCard = $(this).parent().parent().find('.guests').text();
     let roomCard = $(this).parent().parent().find('.room').text();
     let fullCard = $(this).parent().find('.pricefull').text();
     let actualCard = $(this).parent().find('.priceactual').text();
+    let miniImage = $('.hotel__card ').find('.card__image--mini').attr('src');
+    let miniTitle = $('.hotel__card ').find('.info__title--text').text();
+    let miniAdress = $('.hotel__card ').find('.adress').text();
+    let miniGuests = $('.hotel__card ').find('.guests').text();
+    let miniRoom = $('.hotel__card ').find('.roomcard').text();
+    let miniFull= $('.hotel__card ').find('.price__full--text').text();
+    let miniActual = $('.hotel__card ').find('.price__actual--text').text();
     let bigCard = `
-            <div class="hotel__card">
+            <div class="hotel__card active">
               <div class="hotel__card--image">
                 <div class="flash">
                   <span class="flash__text">Flash Offer</span>
@@ -37,15 +44,15 @@ $(document).ready(function () {
                 <div class="characters info__characters">
                   <div class="characters__block">
                     <img src="img/hero/map-pin.svg" alt="map-pin" class="characters__image">
-                    <span class="characters__text">${adressCard}</span>
+                    <span class="characters__text adress">${adressCard}</span>
                   </div>
                   <div class="characters__block">
                     <img src="img/hero/user.svg" alt="user" class="characters__image">
-                    <span class="characters__text">${guestsCard}</span>
+                    <span class="characters__text guests">${guestsCard}</span>
                   </div>
                   <div class="characters__block">
                     <img src="img/hero/home.svg" alt="home" class="characters__image">
-                    <span class="characters__text">${roomCard}</span>
+                    <span class="characters__text roomcard">${roomCard}</span>
                   </div>
                 </div>
                 <!-- /.characters info__characters -->
@@ -72,37 +79,36 @@ $(document).ready(function () {
       <div class="miniflash">
         <span class="miniflash__text">Flash Offer</span>
       </div>
-      <img src="img/hero/hotel1.png" alt="test" class="test-image">
-      <img src="img/hotels/hotel2.png" alt="hotel2" class="minicard__image">
+      <img src="${miniImage}" alt="test" class="test-image">
     </div>
     <!-- /.minihotel--image -->
     <div class="miniinfo minihotel__card--info">
       <div class="minititle info__title">
-        <h2 class="info__title--text">LUX* Belle Mare</h2>
+        <h2 class="info__title--text">${miniTitle}</h2>
       </div>
       <!-- /.info__title -->
       <div class="characters info__characters info__characters--mimi">
         <div class="characters__block">
           <img src="img/hero/map-pin.svg" alt="map-pin" class="characters__image">
-          <span class="characters__text adress">1749 Wheeler Ridge  Delaware</span>
+          <span class="characters__text adress">${miniAdress}</span>
         </div>
         <div class="characters__block">
           <img src="img/hero/user.svg" alt="user" class="characters__image">
-          <span class="characters__text guests">2 x Guests</span>
+          <span class="characters__text guests">${miniGuests}</span>
         </div>
         <div class="characters__block">
           <img src="img/hero/home.svg" alt="home" class="characters__image">
-          <span class="characters__text room">1 x Room</span>
+          <span class="characters__text room">${miniRoom}</span>
         </div>
       </div>
       <!-- /.characters info__characters -->
       <div class="price info__price">
         <div class="price__sum">
           <div class="price__full">
-            <span class="price__full--text pricefull">$ 5,500</span>
+            <span class="price__full--text pricefull">${miniFull}</span>
           </div>
           <div class="price__actual">
-            <span class="price__actual--text priceactual">$ 2,000</span>
+            <span class="price__actual--text priceactual">${miniActual}</span>
           </div>
         </div>
         <button class="price__button price__button--mimi" id="price-button">Book Now</button>
@@ -114,33 +120,17 @@ $(document).ready(function () {
   <!-- /.minihotel__card -->
     `
 
-    if ($(window).find(".hotel__card--info")){
+    if ($(window).find(".hotel__card")){
       $('.hotel__card').parent().removeClass('col-lg-8 col-md-8 col-sm-12 col-12');
       $('.hotel__card').parent().addClass('col-lg-4 col-md-4 col-sm-8');
-      $(this).parent().parent().parent().parent().removeClass('col-lg-4 col-md-4 col-sm-8');
-      $(this).parent().parent().parent().parent().addClass('col-lg-8 col-md-8 col-sm-12 col-12');
+      $('.hotel__card').addClass('active').html(smallCard);
+      // $(this).parent().parent().parent().parent().removeClass('col-lg-4 col-md-4 col-sm-8');
+      $(this).parent().parent().parent().parent().toggleClass('col-lg-4 col-md-4 col-sm-8');
+      $(this).parent().parent().parent().parent().toggleClass('col-lg-8 col-md-8 col-sm-12 col-12').html(bigCard);
+
     }
-
-
-    $(this).parent().parent().parent().html(bigCard);
-    // $('.hotel__card').parent().html(smallCard);
-      // miniShowDialog(smallCard);
-      // showDialog(bigCard);
-
-
-    
     
   });
-
-  // function showDialog(text) {
-  //   $('.small').html(text);
-  // }
-
-  // function miniShowDialog(text) {
-  //   $('.big').html(text);
-  // }
-  
+ 
 });
 
-
-{/* <img src="${imgSrc}" alt="hotel1" class="card__image"> */}
